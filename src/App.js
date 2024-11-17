15f88b7f2f06b8a92a44092fb1e3d04c11575bbc
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-11-17 22:45:54 +0300

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/ConditionRendering.js 	new file:   src/components/Employee.js 	new file:   src/components/EventHandling.js 	new file:   src/components/Greeting.js 	new file:   src/components/HelloWorld.js 	new file:   src/components/MyComponent.js 	new file:   src/components/Student.js 	new file:   src/components/User.js 	new file:   src/components/Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
+import Greeting from './components/Greeting';
+import MyComponent, { FirstComponent, SecondComponent as SC } from './components/MyComponent';
+import HelloWorld from './components/HelloWorld';
+import Student from './components/Student';
+import Employee from './components/Employee';
+import User from './components/User';
+import EventHandling from './components/EventHandling';
+import ConditionRendering from './components/ConditionRendering';
 
 function App() {
+
+  const student = {
+    firstName : "Emrullah",
+    lastName : "Duzgun",
+    email : "emrullah@gmail.com"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          /*
+        <Welcome name="Muhammed"></Welcome>
+        <Greeting name="Muhammed"></Greeting>
+        <FirstComponent></FirstComponent>
+        <SC></SC>
+        <MyComponent></MyComponent>
+        <HelloWorld></HelloWorld>
+        <Student firstName="Muhammed" lastName="Duzgun" email="muhammed@gmail.com"></Student>
+        <Student firstName="Abdullah" lastName="Duzgun" email="abdullah@gmail.com"></Student>
+        <Student student={student}></Student>
+        <Employee></Employee>
+        <User></User>
+        <EventHandling></EventHandling>
+          */
+        }
+      <ConditionRendering></ConditionRendering>
     </div>
   );
 }
